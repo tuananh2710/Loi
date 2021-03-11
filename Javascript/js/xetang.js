3ae8ec3d954d89cae785937e9a28020b7d563ff3
TuanAnh <coanh2710@gmail.com>
2021-03-11 20:20:25 +0700

    Xong

---
+++ Javascript/js/xetang.js
@@ -0,0 +1,13 @@
+function moveUp(marign){
+  var   a=marign;
+   document.getElementById('tank').style.marginTop="-20px";
+}
+function moveLeft(){
+    document.getElementById('tank').style.marginLeft="-20px";
+ }
+ function moveRight(){
+    document.getElementById('tank').style.right="-20px";
+ }
+ function moveDown(){
+    document.getElementById('tank').style.bottom="-20px";
+ }
